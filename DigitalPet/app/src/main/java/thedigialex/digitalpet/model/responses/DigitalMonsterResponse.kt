d2c39177b0e1aa7d29bd7fdcc085006f17b16f87
TheDigiAlex <alejandro11raym@gmail.com>
2024-09-09 15:57:36 -0400

    made changes to the pulling of data

--- DigitalPet/app/src/main/java/thedigialex/digitalpet/model/responses/DigitalMonsterResponse.kt
+++
@@ -1,9 +0,0 @@
-package thedigialex.digitalpet.model.responses
-
-import thedigialex.digitalpet.model.entities.DigitalMonster
-
-data class DigitalMonsterResponse(
-    val status: Boolean,
-    val message: String,
-    val digitalMonsters: List<DigitalMonster>?
-)
--- DigitalPet/app/src/main/java/thedigialex/digitalpet/model/responses/InventoryResponse.kt
+++
@@ -1,9 +0,0 @@
-package thedigialex.digitalpet.model.responses
-
-import thedigialex.digitalpet.model.entities.Inventory
-
-data class InventoryResponse(
-    val status: Boolean,
-    val message: String,
-    val inventory: List<Inventory>
-)
