6a42b8c02b139c33bdf9761792dfe50379e69f24
Alejandro Martinez <alejandro11raym@gmail.com>
2025-04-07 17:24:58 -0400

    update some information

--- digital-monster/resources/views/pages/info.blade.php
+++ digital-monster/resources/views/pages/info.blade.php
@@ -14,7 +14,7 @@
             </x-fonts.paragraph>
         </x-slot>
 
-        <x-accordion title="Getting Started" :open="true" :icon="'fa fa-rocket'">
+        <x-accordion title="Getting Started" :open="true" :icon="'fa-rocket'">
             <x-fonts.paragraph>Welcome! Here's how to begin your journey:</x-fonts.paragraph>
 
             <div class="py-4">
@@ -48,5 +48,40 @@
             </div>
         </x-accordion>
 
+        <x-accordion title="Training" :open="false" :icon="'fa-chart-line'">
+            <x-fonts.paragraph>To rise up the ranks your monsters need those STATS!</x-fonts.paragraph>
+
+            <div class="py-4">
+                <x-fonts.sub-header>1. What are STATS?</x-fonts.sub-header>
+                <x-fonts.paragraph>There are four STATS: Strenght, Agility, Defense, and Mind. 
+                </x-fonts.paragraph>
+                <x-fonts.paragraph>
+                    Each one is important to win battles!
+                </x-fonts.paragraph>
+            </div>
+
+            <div class="py-4">
+                <x-fonts.sub-header>2. How to increase STATS?</x-fonts.sub-header>
+                <x-fonts.paragraph>Clicking on a monster in the DigiGarden will open a stat menu, there will be the four buttons to start training.
+                </x-fonts.paragraph>
+            </div>
+
+            <div class="py-4">
+                <x-fonts.sub-header>3. How does training work?</x-fonts.sub-header>
+                <x-fonts.paragraph>
+                    After clicking start a bar will move, stopping the bar when it's full will give the best stat increase.
+                </x-fonts.paragraph>
+                <x-fonts.paragraph>
+                    Each training equipment can be leveled up increase the overall stat gain.
+                </x-fonts.paragraph>
+            </div>
+
+            <div class="py-4">
+                <x-fonts.sub-header>4. VIRUS, DATA, VACCINE</x-fonts.sub-header>
+                <x-fonts.paragraph>
+                    Each monster type will gain a slight bonus in certain stats.
+                </x-fonts.paragraph>
+            </div>
+        </x-accordion>
     </x-container>
 </x-app-layout>
