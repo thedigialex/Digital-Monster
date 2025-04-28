c3e9c9ad828e2d09264cbe49797330aaa5271cfb
Alejandro Martinez <alejandro11raym@gmail.com>
2025-04-28 18:21:41 -0400

    small change

--- digital-monster/resources/views/pages/user.blade.php
+++ digital-monster/resources/views/pages/user.blade.php
@@ -37,6 +37,7 @@
                 <x-fonts.paragraph><strong>Last Updated At:</strong> {{ $user->updated_at->format('m/d/Y') }} </x-fonts.paragraph>
             </div>
         </div>
+        
 
         <x-accordion title="User Monsters" :open="true" :icon="'fa-dragon'">
             @if (!$user->userMonsters->isEmpty())
